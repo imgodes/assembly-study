d15bfe1ef672b290672daea9db7dc5550958a1e7
Gabriel Guedes <52048434+imgodes@users.noreply.github.com>
2023-06-18 18:37:09 -0300

    Add files via upload

---
+++ bootloader/erro.asm
@@ -0,0 +1,38 @@
+xor bx,bx
+mov ah,0x0e
+
+mov al,'1'
+int 0x10
+
+mov al,segredo
+int 0x10
+; apareceu?
+
+mov al,'2'
+int 0x10
+
+mov al,[segredo]
+int 0x10
+;apareceu(2)?
+
+mov al,'3'
+int 0x10
+
+mov bx,segredo
+add bx,0x7c00
+mov al,[bx]
+xor bx,bx
+int 0x10
+; apareceu (3)?
+
+mov al,'4'
+int 0x10
+
+mov al,[0x7c30]
+int 0x10
+; apareceu (4)?
+
+segredo: db 'X'
+
+times 510 - ($-$$) db 0
+dw 0xAA55
